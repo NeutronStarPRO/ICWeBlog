c3799ce5dca3d0685d532eeb9781516317ea26a2
NeutronStarPRO <15383620197@qq.com>
2022-10-11 20:25:06 +0800

    Add files via upload

---
+++ MyICBlog/src/MyICBlog_assets/src/index.js
@@ -0,0 +1,19 @@
+import { MyICBlog } from "../../declarations/MyICBlog";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await MyICBlog.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
